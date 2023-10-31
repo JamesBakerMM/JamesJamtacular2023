0934b84f3f46cf8c07291b9bf662562b4df88b10
jkorte <jessicakorte@gmail.com>
2023-10-31 21:05:45 +1000

    props are added to array in correct order; able to get substring of props on given layer

--- js/Background.js
+++ js/Background.js
@@ -1,7 +1,7 @@
-const FAR_BACKGROUND = -1;
-const BACKGROUND = 0;
-const FOREGROUND = 1;
-const FAR_FOREGROUND = 2;
+const FAR_BACKGROUND = 1;
+const BACKGROUND = 2;
+const FOREGROUND = 3;
+const FAR_FOREGROUND = 4;
 /*
  * manages the non sprite background elements
  */
@@ -15,27 +15,50 @@ class Background {
         this.visuals.dust = loadImage("./assets/img/bg_dust.png");
     }
     setup() {
-        this.addProp( new Prop(0,0,this.visuals.stars,FAR_BACKGROUND) );
         this.addProp( new Prop(0,0,this.visuals.dust,BACKGROUND) ); 
-        this.sortByLayer();
+        this.addProp( new Prop(0,0,this.visuals.stars,FAR_BACKGROUND) );
+        
     }
     update() {
         for (let prop of this.props) {
             prop.draw();
         }
     }
-    getPropsByLayer(layer){
-        //search through the array, match to .layer return new array with refs just those props
-    }
-    sortByLayer() {
-        //algo that sorts by .layer
+    getPropsByLayer(layer){        
+        let resultArray = [];
+        for (let prop of this.props) {
+            if (prop.layer === layer) {
+                resultArray.push(prop);
+            } else if (resultArray.length > 0) {
+                return resultArray;
+            }
+        }
+        return resultArray;
     }
     addProp(prop) {
         if(prop instanceof Prop===false){
             throw Error("oi that's not a prop! >:|")
         }
-        //search to find the end of the relevant layer in this.props, insert it there, so we don't have to resort the entire array every addition
-        this.props.push(prop); //temp
+        //search to find the end of the relevant layer in this.props, 
+        //insert it there, so we don't have to resort the entire array every addition
+        let index = this.findLastIndexOfProp(prop.layer);
+        this.props.splice(index, 0, prop);        
+    }
+
+    /**
+     * Finds the index at which to insert a new prop; 
+     * i.e. one more than the last prop of the same or lower layer
+     * @param {number} layer    The layer of the prop to insert
+     * @returns The index at which to inset the prop
+     */
+    findLastIndexOfProp(layer) {
+        //debugger;
+        for (let i = this.props.length - 1; i >= 0; i--) {
+            if (this.props[i].layer <= layer) {
+                return i + 1;
+            }
+        }
+        return 0;
     }
 }
 
