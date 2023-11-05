5ab6fe5a8ae0c20e8db21e973b4f53535fc6ecb8
jkorte <jessicakorte@gmail.com>
2023-11-05 21:35:55 +1000

    first create the universe

--- js/Universe.js
+++ js/Universe.js
@@ -1,7 +1,27 @@
 class Universe {
+    constructor() {
+        this.resources;
+        this.factory;    
+    }
+
     preload() {}
 
-    setup() {}
+    setup(factory) {
+        this.factory=factory;
+        this.resources = new Group();
+        for (let i = 0; i < 100; i++) {
+            let x = random(5000);
+            let y = random(5000);
+            this.resources.push(this.factory.createResource(x, y, Math.round(random(4,12))));
+        }
+        this.resources.comets = new Group();
+        let comet = this.factory.createResource(random(-100,0), 
+                random(900,1000), 
+                Math.round(random(20,35))); //Comet POC
+        comet.vel.x = random(1,15);
+        comet.vel.y = -5; //random(-5,0);
+        this.resources.push(comet);
+    }
 
     update() {}
 }
